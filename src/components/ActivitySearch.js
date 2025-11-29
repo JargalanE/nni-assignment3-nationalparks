f3cf00215e9e91c40eace944fa3ada010649fa3c
Jargalan <jargalan0713@gmail.com>
2025-11-29 02:49:06 -0500

    Deploy img issue fix #2

--- src/components/ActivitySearch.js
+++ src/components/ActivitySearch.js
@@ -52,9 +52,10 @@ function ActivitySearch({ onActivitySelected }) {
           <div className="activity-card" key={activity.id}>
             <img
               className="activity-image"
-              src={`icons/${activity.name.toLowerCase().replace(/ /g, "_")}.svg`}
+              src={`/icons/${activity.name.toLowerCase().replace(/ /g, "_")}.svg`}
               alt={activity.name}
             />
+
 
             <button
               className="activity-button"
